744bbe9a8ed12a88ca9b8638cc364d28d3e967a8
Ahmed Roble <airoble2019@gmail.com>
2021-05-17 20:34:49 -0400

    Obtain quote from array and display on click or interval

--- js/script.js
+++ js/script.js
@@ -2,32 +2,78 @@
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
+// Color variables
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Quotes array
+const quotes = [
+  {
+    quote: "Those who dare to fail miserably can achieve greatly.",
+    source: "John F. Kennedy",
+    citation: null,
+    year: null,
+  },
+  {
+    quote: "Make it so!.",
+    source: "Captain Jean Luc Picard",
+    citation: "Star Trek: The Next Generation",
+    year: null,
+  },
+  {
+    quote: "Optimism is the faith that leads to achievement.",
+    source: "Helen Keller",
+    citation: null,
+    year: null,
+  },
+  {
+    quote: "Humility is no substitute for a good personality",
+    source: "Fran Lebowitz",
+    citation: "Metropolitan Life",
+    year: 1978,
+  },
+  {
+    quote: "You do have your moments. Not many, but you have them.",
+    source: "Princess Leia",
+    citation: "The Empire Strikes Back",
+    year: 1980,
+  },
+]
 
-/*** 
- * `quotes` array 
-***/
+// Pick a random quote from array
+const getRandomQuote = function () {
+  const index = Math.floor(Math.random() * quotes.length)
+  return quotes[index]
+}
 
+// display selected quote in DOM
+const printQuote = function () {
+  const quoteObj = getRandomQuote()
+  let HTML = `
+  <p class="quote"> ${quoteObj.quote} </p>
+  <p class="source"> ${quoteObj.source}
+  `
+  if (quoteObj.citation) {
+    HTML += `<span class="citation"> ${quoteObj.citation} </span>`
+  }
+  if (quoteObj.year) {
+    HTML += `<span class="year"> ${quoteObj.year} </span> `
+  }
+  HTML += `</p>`
 
+  document.getElementById("quote-box").innerHTML = HTML
 
-/***
- * `getRandomQuote` function
-***/
+  // color variables
+  let red = Math.floor(Math.random() * 256)
+  let green = Math.floor(Math.random() * 256)
+  let blue = Math.floor(Math.random() * 256)
 
+  document.querySelector(
+    "body"
+  ).style.backgroundColor = `rgb(${red},${green},${blue})`
+}
 
+// Event listener attached to "show" button
+document
+  .getElementById("load-quote")
+  .addEventListener("click", printQuote, false)
 
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+setInterval(printQuote, 2500)
